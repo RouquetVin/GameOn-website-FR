6d0daa05ca3e4f8ec1583751986fa4e3472cf800
Vincent Rouquet <rouquet.v9081@gmail.com>
2024-05-06 10:36:24 +0200

    Modifs HTML / CSS et ajout de close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,31 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById('myTopnav');
+	if (x.className === 'topnav') {
+		x.className += ' responsive';
+	} else {
+		x.className = 'topnav';
+	}
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = 'flex';
 }
 
+const closeM = document.querySelector('.close');
 
+function closeModal(close) {
+	close.addEventListener('click', () => {
+		modalbg.style.display = 'none';
+	});
+}
+
+closeModal(closeM);
